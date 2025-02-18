e5702f64cde231178433a328d944fd61209b6f6e
pratishtha25 <167237939+prats25@users.noreply.github.com>
2025-02-18 23:50:57 +0530

    Add files via upload

---
+++ ProductDemo.java
@@ -0,0 +1,25 @@
+class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void displayProductDetails() {
+        System.out.println("Product Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+    }
+}
+
+public class ProductDemo {
+    public static void main(String[] args) {
+        Product product = new Product(101, "Laptop", 75000);
+        product.displayProductDetails();
+    }
+}
